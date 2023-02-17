c1a21f89956a7d8df53906fdc05e11af96fe46a9
ArtN0nymous <jr616934@gmail.com>
2023-02-16 18:12:34 -0700

    firts commit

---
+++ js/main_firebase.js
@@ -0,0 +1,12 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAiwZTDIUTp45aeZcQI-di4G2PaiaiRQec",
+    authDomain: "sorteosimperial.com.mx",
+    projectId: "sorteosimperial-7bb50",
+    storageBucket: "sorteosimperial-7bb50.appspot.com",
+    messagingSenderId: "227251814603",
+    appId: "1:227251814603:web:0ae7f88e1f24b4f5ba5b4e",
+    measurementId: "G-R5TLGM9ELB"
+});
+var db = firebase.firestore();
+var storageRef = firebase.storage();
+//const analytics = firebase.analytics();
